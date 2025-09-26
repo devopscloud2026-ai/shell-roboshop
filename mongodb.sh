b6cae5e1f8ed62247cee6caac35f6f682726986e
althaf <shaikalthaf1984@gmail.com>
2025-09-26 19:58:56 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -28,7 +28,7 @@ VALIDATE(){
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding Mongo repo"
 
-dnf install mongodb-org -y &>>$LOG_FILE
+dnf install mongodb-org -y 
 VALIDATE $? "Installing MongoDB"
 
 systemctl enable mongodb &>>$LOG_FILE

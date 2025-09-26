5f412012ebca81170c54a801873392eec3c61454
althaf <shaikalthaf1984@gmail.com>
2025-09-26 19:27:26 +0530

    roboshop

---
+++ mongodb.sh
@@ -0,0 +1,38 @@
+USERID=$(id -u)
+R="e\[31m"
+G="e\[32m"
+Y="e\[33m"
+N="e\[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script start executed at: $(date)"
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root Privelege"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2...$R is failure $N"
+        exit 1
+    else
+        echo -e "$2...$G success $N"
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enable MongoDB" 
+
+systemctl start mongod 
+VALIDATE $? "Start MongoDB" 

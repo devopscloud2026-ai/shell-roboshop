7d1e896c755fd791e2363d47695f607a97a52f4c
althaf <shaikalthaf1984@gmail.com>
2025-09-26 20:03:44 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -25,14 +25,14 @@ VALIDATE(){
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Adding Mongo repo"
+    cp mongo.repo /etc/yum.repos.d/mongo.repo
+    VALIDATE $? "Adding Mongo repo"
 
-dnf install mongodb-org -y &>>$LOG_FILE
-VALIDATE $? "Installing MongoDB"
+    dnf install mongodb-org -y &>>$LOG_FILE
+    VALIDATE $? "Installing MongoDB"
 
-systemctl enable mongodb &>>$LOG_FILE
-VALIDATE $? "Enable MongoDB" 
+    systemctl enable mongodb &>>$LOG_FILE
+    VALIDATE $? "Enable MongoDB" 
 
-systemctl start mongodb
-VALIDATE $? "Start MongoDB" 
+    systemctl start mongodb
+    VALIDATE $? "Start MongoDB" 
